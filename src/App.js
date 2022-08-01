78e03d04800de29200be56c8f7d1da092d17cafb
Ian Iversen <ianiversen1@gmail.com>
2022-08-01 15:57:19 -0400

    deletions and server update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ function App() {
   
   return (
     <div className="App">
-      <card>
-        <img src='https://randomuser.me/api/portraits/men/58.jpg' alt='name' />
-        <p>location: city: 'Kinsarvik', state: 'Troms - Romsa', country: 'Norway', postcode: '3120' </p>
-        <p>Name": "title: 'Mr', first: 'Zborislav', last: 'Rudnickiy</p>
-        <p>Age: 67</p>
-        <p>Email: "agnethe.gronnestad@example.com</p>
-      </card>
+      <>Hello</>
     </div>
   );
 }
