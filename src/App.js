3509e0ba02d9aff87ce51661e28b0647fe178442
Rabah <rmerzoug471@gmail.com>
2022-08-02 09:58:02 -0500

    userForm post

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import './App.css';
+import './index.css';
 import UserForm from './UserForm';
 import ProfileContainer from './ProfileContainer';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [users, setUsers] = useState([])
+
   useEffect(() => {
     fetch('http://localhost:3001/users')
     .then( res => res.json())
     .then( users => setUsers(users) )
   },[])
 
+  function submitFunction(obj) {
+    setUsers([...users, obj])
+  }
+
   
   return (
     <div className="App">
       <>Hello</>
-     
+      <UserForm submitFunction={submitFunction} />
       <ProfileContainer users={users} />
     </div>
   );
