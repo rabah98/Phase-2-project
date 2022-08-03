baadaa6898704fcf5a57365a3bdc59a1e221923e
Rabah <rmerzoug471@gmail.com>
2022-08-03 09:54:46 -0500

    user details

--- src/Components/ProfileContainer.js
+++ src/Components/ProfileContainer.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import UserCard from './UserCard';
+import { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 const ProfileContainer = ({users}) => {
-    const allUsers = users.map( user => <UserCard key={user.id} image={user.image} name={user.name} /> )
+    const allUsers = users.map( user => <UserCard key={user.id}
+        image={user.image}
+        name={user.name} 
+        location={user.location}
+        email={user.email}
+        age={user.age} 
+        showFunction={showFunction} 
+    /> )
+
+    const [age, setAge] = useState('')
+    const [email, setEmail] = useState('')
+    const [location, setLocation] = useState('')
+    
+    function showFunction(str1, str2, str3) {
+        setEmail(str1)
+        setAge(str2)
+        setLocation(str3)
+    }
     return (
         <div className="cards">
             {allUsers}
+            <div>
+                <p>{age}</p>
+                <p>{location}</p>
+                <p>{email}</p> 
+            </div>
+           
         </div>
     );
 };
--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const UserCard = ({name, image}) => {
+const UserCard = ({name, image, email, age, location, showFunction}) => {
+    function handleClick () {
+        showFunction(email, age, location)
+    }
     return (
         <div className="card" >
-            <img src={image} alt={name} />
+            <img src={image} alt={name} onClick={handleClick} />
             <p>{name}</p>
         </div>
     );
